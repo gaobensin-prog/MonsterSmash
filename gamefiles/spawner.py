a372009a1123b923e0133bd4db8e7896e4d87d48
Zhi B Gao <gaobensin@gmail.com>
2026-08-31 22:17:39 -0500

    Add a way to reset the list of enemy in spawner so that the data dont get too big

--- gamefiles/spawner.py
+++ gamefiles/spawner.py
@@ -9,11 +9,18 @@ class Spawner(Gameobject):
         self.__list = []
         self.__dt = 0
     def update(self, dt):
-        self.__dt += (dt / 10)
+        self.__dt += (dt * 10)
         if self.__dt > 1:
             enemy = Enemy(screen_width / 2, screen_length / 2)
             self.__list.append(enemy)
             self.__dt = 0
     def draw(self, screen):
+        alive = []
+        for enemy in self.__list:
+            if enemy.health > 0:
+                alive.append(enemy)
+        self.__list = alive 
         for i in range(len(self.__list)):
-            self.__list[i].draw(screen)
+            if self.__list[i].health > 0:
+                self.__list[i].draw(screen)
+        alive = []
