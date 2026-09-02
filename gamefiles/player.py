e27a1fe43c6c348292a1bea5002de183cbd15e52
Zhi B Gao <gaobensin@gmail.com>
2026-09-02 14:23:02 -0500

    Test Healthbar

--- gamefiles/player.py
+++ gamefiles/player.py
@@ -9,6 +9,7 @@ class Player(Gameobject):
         self.__picture = image
         self.health = 1000
         self.rect = pygame.Rect(x, y, width_of_entity, height_of_entity)
+        self.rect_hp = pygame.Rect(0, 1280,  width_of_entity, height_of_entity)
     def update(self, enemy):
         keys = pygame.key.get_pressed()
         if self.health <= 0:
@@ -16,19 +17,22 @@ class Player(Gameobject):
             return
         if keys[pygame.K_a]:
             self.rect.x -= 10
+            self.rect_hp.x -= 10
         if keys[pygame.K_d]:
             self.rect.x += 10
+            self.rect_hp.x += 10
         if keys[pygame.K_s]:
             self.rect.y += 10
+            self.rect_hp.y += 10
         if keys[pygame.K_w]:
             self.rect.y -= 10
+            self.rect_hp.y -= 10
         if (
             self.rect.x - width_of_entity / 2  <= enemy.rect.x + width_of_entity / 2
             and self.rect.x + width_of_entity / 2 >= enemy.rect.x - width_of_entity / 2
             and self.rect.y - height_of_entity / 2 <= enemy.rect.y + height_of_entity / 2
             and self.rect.y + height_of_entity / 2 >= enemy.rect.y - height_of_entity / 2
         ):
-            print(self.health)
             self.change_stat(self.health, enemy.attack, self.defense)
         
     
@@ -38,7 +42,13 @@ class Player(Gameobject):
         sizeh = height_of_entity - pic.get_height()
         player_img = pygame.transform.scale(pic, (pic.get_width() + sizew, pic.get_height() + sizeh))
         screen.blit(player_img, self.rect)
+        self.health_bar(screen)
 
     def get_stuff(self, stuff: str) -> tuple:
         if stuff.lower() == "image":
             return Picture(self.__picture).get_picture()
+    
+    def health_bar(self, screen):
+        health = pygame.font.Font(None, 32)
+        surface = health.render(str(round(self.health)), False, "red")
+        screen.blit(surface, (self.rect_hp.x, self.rect_hp.y))
