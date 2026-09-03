bd2d0f5b5b19d3b9b20188cf1eb30975fcf4274b
Zhi B Gao <gaobensin@gmail.com>
2026-09-02 22:16:15 -0500

    Add attackable group so that it can rotate through the using attack method

--- gamefiles/main.py
+++ gamefiles/main.py
@@ -16,9 +16,10 @@ def main():
     #groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Enemy.containers = (updatable, drawable)
+    attackable = pygame.sprite.Group()
+    Enemy.containers = (updatable, drawable, attackable)
     enemy = Enemy(screen)
-    Player.containers = (updatable, drawable)
+    Player.containers = (updatable, drawable, attackable)
     player = Player(screen, 50, 50)
     Spawner.containers = (updatable, drawable)
     spawner = Spawner(screen)
@@ -41,7 +42,7 @@ def main():
             elif isinstance(thing, Spawner):
                 thing.update(dt, screen)
             elif isinstance(thing, Player):
-                thing.update(enemy)
+                thing.update()
             else:
                 thing.update()
         for thing in drawable:
@@ -49,6 +50,10 @@ def main():
                 thing.draw(screen, dt)
             else:
                 thing.draw(screen)
+        for thing in attackable:
+            for target in attackable:
+                if target is not thing:
+                    thing.attacking(target, dt)
         #put everything on the screen
         pygame.display.flip()
         dt = clock.tick(60) / 1000
