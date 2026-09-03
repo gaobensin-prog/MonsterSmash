0547d267f874dfd619a3093258b241fc0484db90
Zhi B Gao <gaobensin@gmail.com>
2026-09-03 10:21:04 -0500

    Pausing the game for 5 seconds

--- gamefiles/wave.py
+++ gamefiles/wave.py
@@ -1,2 +1,11 @@
-class Wave():
-    pass
+from gameobject import Gameobject
+import pygame
+class Wave(Gameobject):
+    def __init__(self,time):
+        super().__init__()
+        self.time = time
+
+    def update(self, time):
+        self.time = time.time
+        if self.time <= 0:
+            pygame.time.delay(5000)
